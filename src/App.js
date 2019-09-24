c09b42c90ea5c0e22bb51ab4cd0d5cfdfd698522
Joe <josephc.lambert@gmail.com>
2019-09-24 11:18:27 -0700

    adds map link to navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginForm from './Login';
 import SignupForm from './Signup';
 import { Switch, Route } from 'react-router-dom';
 import Map from './components/map/Map';
 import UserProfile from './components/user/userProfile';
 import PrivateRoute from './privateRoute';
+import NavBar from './components/layout/navBar';
 
 function App() {
+  const [user, setUser] = useState({
+    isLoggedIn: true,
+    userName: 'losephjambert',
+  });
   return (
     <>
+      <NavBar user={user} />
       <Switch>
         <Route exact path='/' component={Map} />
         <Route path='/login' render={props => <LoginForm {...props} />} />
