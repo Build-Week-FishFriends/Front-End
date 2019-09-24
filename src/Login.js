6f8f74c41d017e767958bb92f4963a5fb7059a40
Joe <josephc.lambert@gmail.com>
2019-09-24 10:08:34 -0700

    fixes material import error:

--- src/Login.js
+++ src/Login.js
@@ -1,68 +1,58 @@
-import React, { useState, useEffect } from "react";
-import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 import axiosWithAuth from './WithAuth';
 
-const BasicLoginForm= ({ values, errors, touched, status })=>{
-    const [peopleInfo, setInfo]= useState([])
-    useEffect(() => {
-        if (status) {
-          setInfo([...peopleInfo, status]);
-        }
-      }, [status]);
-    return(
+const BasicLoginForm = ({ values, errors, touched, status }) => {
+  const [peopleInfo, setInfo] = useState([]);
+  useEffect(() => {
+    if (status) {
+      setInfo([...peopleInfo, status]);
+    }
+  }, [status]);
+  return (
+    <div>
+      <Form className='Form'>
+        <Field value={values.username} className='Fields' type='text' name='username' placeholder='Username/Email' />
+        {touched.username && errors.username && <p>{errors.username}</p>}
+        <Field value={values.pass} className='Fields' type='text' name='pass' placeholder='Password' />
+        {touched.pass && errors.pass && <p>{errors.pass}</p>}
         <div>
-            <Form className='Form'>
-                <Field value={values.username} className='Fields' type='text' name='username' placeholder='Username/Email'/>
-                {touched.username && errors.username && (
-                <p>{errors.username}</p>
-                )}
-                <Field value={values.pass} className='Fields' type='text' name='pass' placeholder='Password'/>
-                {touched.pass && errors.pass && (
-                <p>{errors.pass}</p>
-                )}
-                <div>
-                    <button type="submit" value="Submit">
-                        Login
-                    </button>
-                    <p>Or</p>
-                    <button>
-                        Sign Up
-                    </button>
-                </div>
-            </Form>
+          <button type='submit' value='Submit'>
+            Login
+          </button>
+          <p>Or</p>
+          <button>Sign Up</button>
         </div>
-    )
-}
+      </Form>
+    </div>
+  );
+};
 
-const LoginForm= withFormik({
-    mapPropsToValues({ username, pass}) {
-        return {
-          username: username || '',
-          pass: pass || ''
-        };
-      },
-    validationSchema: Yup.object().shape({
-    username: Yup.string()
-    .required('Name is required'),
-    pass: Yup.string()
-    .required('Password is required')
-    }),
-    handleSubmit(values, { setStatus }) {
-        const {username,pass}= values
-        const postValues= {username, password:pass}
-        console.log(postValues)
-        axios
-          .post('https://fish-friends.herokuapp.com/auth/login', postValues)
-          .then(response => {
-            console.log(response)
-            setStatus(response.data)
-            localStorage.setItem('token', response.data.token)
-          })
-          .catch(error => console.log('Error'));
-      }
-})(BasicLoginForm)
-export default LoginForm
+const LoginForm = withFormik({
+  mapPropsToValues({ username, pass }) {
+    return {
+      username: username || '',
+      pass: pass || '',
+    };
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required('Name is required'),
+    pass: Yup.string().required('Password is required'),
+  }),
+  handleSubmit(values, { setStatus }) {
+    const { username, pass } = values;
+    const postValues = { username, password: pass };
+    console.log(postValues);
+    axios
+      .post('https://fish-friends.herokuapp.com/auth/login', postValues)
+      .then(response => {
+        console.log(response);
+        setStatus(response.data);
+        localStorage.setItem('token', response.data.token);
+      })
+      .catch(error => console.log('Error'));
+  },
+})(BasicLoginForm);
+export default LoginForm;
--- src/Signup.js
+++ src/Signup.js
@@ -1,97 +1,80 @@
-import React, { useState, useEffect } from "react";
-import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 import axiosWithAuth from './WithAuth';
 
-const BasicSignupForm= ({ values, errors, touched, status })=>{
-    const [peopleInfo, setInfo]= useState([])
-    useEffect(() => {
-        if (status) {
-          setInfo([...peopleInfo, status]);
-        }
-      }, [status]);
-    return(
+const BasicSignupForm = ({ values, errors, touched, status }) => {
+  const [peopleInfo, setInfo] = useState([]);
+  useEffect(() => {
+    if (status) {
+      setInfo([...peopleInfo, status]);
+    }
+  }, [status]);
+  return (
+    <div>
+      <Form className='Form'>
+        <Field value={values.firstName} className='Fields' type='text' name='firstName' placeholder='First Name' />
+        {touched.firstname && errors.firstname && <p>{errors.firstname}</p>}
+        <Field value={values.lastName} className='Fields' type='text' name='lastName' placeholder='Last Name' />
+        {touched.lastname && errors.lastname && <p>{errors.lastname}</p>}
+        <Field value={values.username} className='Fields' type='text' name='username' placeholder='Username' />
+        {touched.username && errors.username && <p>{errors.username}</p>}
+        <Field value={values.email} className='Fields' type='text' name='email' placeholder='Email' />
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field value={values.pass} className='Fields' type='text' name='pass' placeholder='Password' />
+        {touched.pass && errors.pass && <p>{errors.pass}</p>}
+        <Field value={values.passconf} className='Fields' type='text' name='passconf' placeholder='Confirm Password' />
+        {touched.passconf && errors.passconf && <p>{errors.passconf}</p>}
         <div>
-            <Form className='Form'>
-                <Field value={values.firstName} className='Fields' type='text' name='firstName' placeholder='First Name'/>
-                {touched.firstname && errors.firstname && (
-                <p>{errors.firstname}</p>
-                )}
-                <Field value={values.lastName} className='Fields' type='text' name='lastName' placeholder='Last Name'/>
-                {touched.lastname && errors.lastname && (
-                <p>{errors.lastname}</p>
-                )}
-                <Field value={values.username} className='Fields' type='text' name='username' placeholder='Username'/>
-                {touched.username && errors.username && (
-                <p>{errors.username}</p>
-                )}
-                <Field value={values.email} className='Fields' type='text' name='email' placeholder='Email'/>
-                {touched.email && errors.email && (
-                <p>{errors.email}</p>
-                )}
-                <Field value={values.pass} className='Fields' type='text' name='pass' placeholder='Password'/>
-                {touched.pass && errors.pass && (
-                <p>{errors.pass}</p>
-                )}<Field value={values.passconf} className='Fields' type='text' name='passconf' placeholder='Confirm Password'/>
-                {touched.passconf && errors.passconf && (
-                <p>{errors.passconf}</p>
-                )}
-                <div>
-                    <button type="submit" value="Submit">
-                        Submit
-                    </button>
-                </div>
-            </Form>
+          <button type='submit' value='Submit'>
+            Submit
+          </button>
         </div>
-    )
-}
+      </Form>
+    </div>
+  );
+};
 
-const SignupForm= withFormik({
-    mapPropsToValues({ firstName, lastName, username, email, pass, passconf}) {
-        return {
-          firstName: firstName || '',
-          lastName: lastName || '',
-          username: username || '',
-          email: email || '',
-          pass: pass || '',
-          passconf: passconf || ''
-        };
-      },
-    validationSchema: Yup.object().shape({
-    firstName: Yup.string()
-    .required('First name is required'),
-    lastName: Yup.string()
-    .required('Last name is required'),
-    username: Yup.string()
-    .required('Username is required'),
-    email: Yup.string()
-    .required('Email is required'),
+const SignupForm = withFormik({
+  mapPropsToValues({ firstName, lastName, username, email, pass, passconf }) {
+    return {
+      firstName: firstName || '',
+      lastName: lastName || '',
+      username: username || '',
+      email: email || '',
+      pass: pass || '',
+      passconf: passconf || '',
+    };
+  },
+  validationSchema: Yup.object().shape({
+    firstName: Yup.string().required('First name is required'),
+    lastName: Yup.string().required('Last name is required'),
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().required('Email is required'),
     pass: Yup.string()
-    .min(5, 'Password must be at least 5 characters')
-    .max(15, 'Password cannot exceed 15 characters')
-    .required('A password is required'),
+      .min(5, 'Password must be at least 5 characters')
+      .max(15, 'Password cannot exceed 15 characters')
+      .required('A password is required'),
     passconf: Yup.string()
-    .required('Please validate your password')
-    .test('',  'Passwords do not match', function(value){
-        return this.parent.pass === value
-    })
-    }),
-    handleSubmit(values, { setStatus }) {
-      const {firstName,lastName,username,email,pass}= values
-      const postValues= {firstName, lastName, username, email, password:pass}
-        axios
-          .post('https://fish-friends.herokuapp.com/auth/register', postValues)
-          .then(response => {
-            console.log(response.data)
-            setStatus(response.data);
-            localStorage.setItem('token', response.data.token)
-          })
-          .catch(error => console.log('Error'));
-      }
-})(BasicSignupForm)
+      .required('Please validate your password')
+      .test('', 'Passwords do not match', function(value) {
+        return this.parent.pass === value;
+      }),
+  }),
+  handleSubmit(values, { setStatus }) {
+    const { firstName, lastName, username, email, pass } = values;
+    const postValues = { firstName, lastName, username, email, password: pass };
+    axios
+      .post('https://fish-friends.herokuapp.com/auth/register', postValues)
+      .then(response => {
+        console.log(response.data);
+        setStatus(response.data);
+        localStorage.setItem('token', response.data.token);
+      })
+      .catch(error => console.log('Error'));
+  },
+})(BasicSignupForm);
 
 // function Signup() {
 //   const [credentials, setCredentials] = useState({username: '', password: ''})
@@ -110,12 +93,12 @@ const SignupForm= withFormik({
 //   }
 
 //   const handleChange = e => {
-//       console.log(credentials) 
+//       console.log(credentials)
 //       setCredentials({...credentials, [e.target.name]: e.target.value})
 //   }
 
 //   return (
-     
+
 //           <Label>Username</Label>
 //               <Input
 //                   type='text'
@@ -124,12 +107,12 @@ const SignupForm= withFormik({
 //                   onChange={handleChange}
 //               />
 //               <Label>Password</Label>
-//               <Input 
+//               <Input
 //                   type='password'
 //                   name='password'
 //                   value={credentials.password}
 //                   onChange={handleChange}
 //               />
 //               <Button>Submit!</Button>
-  
+
 export default SignupForm;
