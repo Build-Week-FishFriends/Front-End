8deb5331e0710325e219d2664956add38da29608
Joe <josephc.lambert@gmail.com>
2019-09-24 16:55:14 -0700

    wip

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import ReactMapGL from 'react-map-gl';
 import Lakes from './Lakes';
 
+export default function Map({ handleLakes }) {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 47.605514,
+    longitude: -121.668538,
+    zoom: 5,
+  });
 
-export default class Map extends Component {
-  state = {
-    viewport: {
-      width: '100vw',
-      height: '100vh',
-      latitude: 47.605514,
-      longitude: -121.668538,
-      zoom: 5,
-    },
-  };
-
-  render() {
-    return (
-      <ReactMapGL
-        {...this.state.viewport}
-        mapboxApiAccessToken={
-          'pk.eyJ1IjoibG9zZXBoamFtYmVydCIsImEiOiJjazB3cG1mNnAxY2ltM21wb2JsdnUzajNsIn0.BLFLlkgEnpDe553A9dmFAA'
-        }
-        onViewportChange={viewport => this.setState({ viewport })}>
-        <ul>
-          <Lakes zoom={this.state.zoom} />
-        </ul>
-      </ReactMapGL>
-    );
-  }
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={
+        'pk.eyJ1IjoibG9zZXBoamFtYmVydCIsImEiOiJjazB3cG1mNnAxY2ltM21wb2JsdnUzajNsIn0.BLFLlkgEnpDe553A9dmFAA'
+      }
+      onViewportChange={viewport => setViewport({ viewport })}>
+      <ul>
+        <Lakes zoom={viewport.zoom} />
+      </ul>
+    </ReactMapGL>
+  );
 }
