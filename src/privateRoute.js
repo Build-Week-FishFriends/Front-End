c3b1bce87abff41b6ac82f7367e2922ee6d8d735
Joe <josephc.lambert@gmail.com>
2019-09-24 17:27:26 -0700

    remove unused console log

--- src/privateRoute.js
+++ src/privateRoute.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 function PrivateRoute({ component: Component, ...rest }) {
-  console.log(rest);
   return (
     <Route
       {...rest}
