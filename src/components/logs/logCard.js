68020f6ecca17248ca54a8d9be0b1bca8c356d7a
Joe <josephc.lambert@gmail.com>
2019-09-25 15:27:39 -0700

    styles log card

--- src/components/logs/logCard.js
+++ src/components/logs/logCard.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import DeleteLog from './deleteLog';
 
+const StyledLogCard = styled.li`
+  list-style: none;
+  margin: 10px;
+  padding: 15px;
+  box-shadow: 0 0 3px 0px black;
+  border-radius: 3px;
+  h3 {
+    font-size: 1.6rem;
+  }
+`;
+
 const LogCard = ({ handleDeleteLog, log }) => (
-  <li>
+  <StyledLogCard>
     <h3>{log.facilityName}</h3>
-    <p>{log.fishName}</p>
-    <p>{log.fishCount}</p>
-    <p>{log.baitType}</p>
-    <p>{log.timeSpent}</p>
-    <p>{log.timeOfDay}</p>
+    <div>
+      <p>
+        Fish caught: {log.fishCount} {log.fishName}
+      </p>
+    </div>
+    <div>
+      <p>Bait used: {log.baitType}</p>
+    </div>
+    <div>
+      <p>Time spent: {log.timeSpent}</p>
+    </div>
+    <div>
+      <p>Time of day: {log.timeOfDay}</p>
+    </div>
     <DeleteLog handleDeleteLog={handleDeleteLog} id={log.log_id} />
-  </li>
+  </StyledLogCard>
 );
 export default LogCard;
