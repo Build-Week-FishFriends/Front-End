38fadcb2ee8ae1c3780a639539c7deccf2f0ae31
Joseph Lambert <josephc.lambert@gmail.com>
2019-09-25 15:38:37 -0700

    Joe lambert/userprofile (#16)
    * user profile makes request to get logs by id and handles reqs
    
    * renders simple log card if the user has logs
    
    * delete log form successfully deletes log
    
    * moves delete log and log card into own components
    
    * styles log card
    
    * finishes log card style
    
    * conditionally renders log card list header

---
+++ src/components/logs/logCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import DeleteLog from './deleteLog';
+
+const StyledLogCard = styled.li`
+  list-style: none;
+  margin: 10px;
+  padding: 15px;
+  box-shadow: 0 0 3px 0px black;
+  border-radius: 3px;
+
+  h3 {
+    font-size: 1.6rem;
+  }
+
+  button {
+    width: 75%;
+    margin-top: 50px;
+  }
+`;
+
+const LogCard = ({ handleDeleteLog, log }) => (
+  <StyledLogCard>
+    <h3>{log.facilityName}</h3>
+    <div>
+      <p>
+        Fish caught: {log.fishCount} {log.fishName}
+      </p>
+    </div>
+    <div>
+      <p>Bait used: {log.baitType}</p>
+    </div>
+    <div>
+      <p>Time spent: {log.timeSpent}</p>
+    </div>
+    <div>
+      <p>Time of day: {log.timeOfDay}</p>
+    </div>
+    <DeleteLog handleDeleteLog={handleDeleteLog} id={log.log_id} />
+  </StyledLogCard>
+);
+export default LogCard;
--- src/components/user/userProfile.js
+++ src/components/user/userProfile.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import axiosWithAuth from '../auth/WithAuth';
+import LogCard from '../logs/logCard';
 
-const UserProfile = () => {
+const UserProfile = ({ user, match, history, location }) => {
+  const [logs, setLogs] = useState([]);
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/logRoute/user-logs/`, {
+        params: {
+          id: user.userId,
+        },
+      })
+      .then(res => {
+        setLogs([...res.data]);
+      })
+      .catch(err => {
+        console.log(err);
+        console.error(err);
+      });
+  }, [user.userId]);
+
+  useEffect(() => {});
+
+  const deleteLog = (e, log_id) => {
+    e.preventDefault();
+    console.log('delete log');
+    axiosWithAuth()
+      .delete(`/logRoute/user-logs/delete-logs/${log_id}`)
+      .then(res => {
+        console.log(res.data);
+        const filteredLogs = logs.filter(log => {
+          return log.log_id !== log_id;
+        });
+        console.log('filtered logs', filteredLogs);
+        setLogs(filteredLogs);
+      })
+      .catch(err => {
+        console.log(err);
+        console.error(err);
+      });
+  };
+
   return (
-    <div>
-      <div>This is the user profile</div>
-    </div>
+    <section>
+      <h2>
+        Hello {user.firstName} {user.lastName}
+      </h2>
+      <section>
+        {logs.length > 0 && <h2>Your Logs</h2>}
+        <ul>
+          {logs.map(log => (
+            <LogCard key={log.log_id} handleDeleteLog={deleteLog} log={log} />
+          ))}
+        </ul>
+      </section>
+    </section>
   );
 };
 
