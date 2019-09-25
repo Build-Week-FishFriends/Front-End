f5f07ad88dc8767697188c10dfc5a113843735d5
Joe <josephc.lambert@gmail.com>
2019-09-25 10:49:14 -0700

    removes mapLock function

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,43 +1,35 @@
-import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
-import Lakes from "./Lakes";
-import PrivateRoute from "../../privateRoute";
-import LakesCard from "../LakesCard";
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+import Lakes from './Lakes';
+import PrivateRoute from '../../privateRoute';
+import LakesCard from '../LakesCard';
 export default function Map({ lakes }) {
-  const [lockMap, setLockMap] = useState(false);
   const [viewport, setViewport] = useState({
-    width: "100vw",
-    height: "100vh",
+    width: '100vw',
+    height: '100vh',
     latitude: 47.605514,
     longitude: -121.668538,
-    zoom: 5
+    zoom: 5,
   });
 
   const _onViewportChange = viewport => {
-    !lockMap && setViewport({ ...viewport });
+    setViewport({ ...viewport });
   };
-
-  const handleLockMap = () => setLockMap(!lockMap);
 
   return (
     <>
       <ReactMapGL
-        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapStyle='mapbox://styles/mapbox/dark-v9'
         {...viewport}
         mapboxApiAccessToken={
-          "pk.eyJ1IjoibG9zZXBoamFtYmVydCIsImEiOiJjazB3cG1mNnAxY2ltM21wb2JsdnUzajNsIn0.BLFLlkgEnpDe553A9dmFAA"
+          'pk.eyJ1IjoibG9zZXBoamFtYmVydCIsImEiOiJjazB3cG1mNnAxY2ltM21wb2JsdnUzajNsIn0.BLFLlkgEnpDe553A9dmFAA'
         }
-        onViewportChange={_onViewportChange}
-      >
+        onViewportChange={_onViewportChange}>
         <ul>
-          <Lakes
-            zoom={viewport.zoom}
-            lakes={lakes}
-            handleLockMap={handleLockMap}
-          />
+          <Lakes zoom={viewport.zoom} lakes={lakes} />
         </ul>
       </ReactMapGL>
-      <PrivateRoute path="/map/:id" component={LakesCard} />
+      <PrivateRoute path='/map/:id' component={LakesCard} />
     </>
   );
 }
