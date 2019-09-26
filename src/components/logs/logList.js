c775f18a252de696cb3211420c792f41379d5846
Joe <josephc.lambert@gmail.com>
2019-09-26 15:19:54 -0700

    removes unused code in logList

--- src/components/logs/logList.js
+++ src/components/logs/logList.js
@@ -1,19 +1,12 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-
-const LogList = (props) => {
+const LogList = ({ id }) => {
   const [logs, setLogs] = useState([]);
-  const {id} = props;
-
-  console.log("idd", id)
 
   useEffect(() => {
-    console.log("Logs List, use effect");
     axios
-      .get(
-        `https://fish-friends.herokuapp.com/logRoute/user-logs/waterBody/${id}`
-      )
+      .get(`https://fish-friends.herokuapp.com/logRoute/user-logs/waterBody/${id}`)
       .then(res => {
         console.log(res.data);
         setLogs(res.data);
@@ -29,14 +22,12 @@ const LogList = (props) => {
       {logs.map((log, i) => {
         return (
           <li key={i}>
-           
-           
             <div>
-            <h1>{log.username}</h1>
+              <h1>{log.username}</h1>
               <ul>
                 <li>Fish caught : {log.fishName}.</li>
                 <li>Fish Amount : {log.fishCount}</li>
-                <li>Time spent  : {log.timeSpent}</li>
+                <li>Time spent : {log.timeSpent}</li>
                 <li>Time Of Day : {log.timeOfDay}</li>
               </ul>
             </div>
